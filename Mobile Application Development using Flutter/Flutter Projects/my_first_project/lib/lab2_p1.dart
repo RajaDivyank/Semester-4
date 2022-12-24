7ec5f5c54923c3e90d63b9d8df7177d6824b951d
Divyank Raja <107948268+RajaDivyank@users.noreply.github.com>
2022-12-24 20:17:19 +0530

    dart

--- Mobile Application Development using Flutter/Flutter Projects/my_first_project/lib/lab2_p1.dart
+++
@@ -1,8 +0,0 @@
-// import 'package:flutter/cupertino.dart';
-//
-// class Lab2_p1 extends StatelessWidget{
-//   @override
-//   Widget build(BuildContext text){
-//     return scaffold()
-//   }
-// }
---
+++ Mobile Application Development using Flutter/Flutter Projects/my_first_project/lib/lab7_1.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(Lab7_1());
+class Lab7_1 extends StatelessWidget{
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Text("Hello"),
+    );
+  }
+
+  // @override
+  // State<StatefulWidget> createState() {
+  //   TODO: implement createState
+  //   throw UnimplementedError();
+  }
+}
