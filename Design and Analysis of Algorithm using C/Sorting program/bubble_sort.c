0d076f4e4719f5f13d61275967b73172e09ec619
Divyank Raja <107948268+RajaDivyank@users.noreply.github.com>
2022-12-27 20:28:58 +0530

    C - language

--- Design and Analysis of Algorithm using C/Sorting program/bubble_sort.c
+++ Design and Analysis of Algorithm using C/Sorting program/bubble_sort.c
@@ -1,19 +1,38 @@
 #include <stdio.h>
+#include<time.h>
 
 void main()
 {
-    FILE *fp;
-    fp = fopen("data.txt", "r");
-    int a[100000], i, j, temp, n;
-    printf("Enter Number : ");
-    scanf("%d", &n);
-    for (i = 0; i < n; i++)
+    double time_spent = 0.0;
+    clock_t begin, end;
+    begin = clock();
+    FILE *fp, *fo;
+    int a[100000], i, j, temp, ch;
+    fo = fopen("output.txt", "w");
+    printf("Enter the choice : 1:Best case\n 2:Average case\n 3:Worst case\n");
+    scanf("%d", &ch);
+    switch (ch)
+    {
+    case 1:
+        fp = fopen("best.txt", "r");
+        break;
+    case 2:
+        fp = fopen("average.txt", "r");
+        break;
+    case 3:
+        fp = fopen("worst.txt", "r");
+        break;
+    default:
+        printf("Invalid choice");
+        break;
+    }
+    for (i = 0; i < 100000; i++)
     {
         fscanf(fp, "%d", &a[i]);
     }
-    for (int i = 0; i < n - 1; i++)
+    for (int i = 0; i < 100000 - 1; i++)
     {
-        for (j = 0; j < n - i - 1; j++)
+        for (j = 0; j < 100000 - i - 1; j++)
         {
             if (a[j] > a[j + 1])
             {
@@ -23,8 +42,12 @@ void main()
             }
         }
     }
-    for (i = 0; i < n; i++)
+    for (i = 1; i <= 100000; i++)
     {
-        printf("%d\n", a[i]);
+        fprintf(fo, "%d\n", i);
     }
+
+    end = clock();
+    time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Time For Code = %lf ", time_spent);
 }
