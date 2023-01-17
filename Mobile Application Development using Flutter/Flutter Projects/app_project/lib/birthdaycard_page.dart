1bca17ffd1fcfb0e5ed147c146a4d71cbae147ba
Divyank Raja <107948268+RajaDivyank@users.noreply.github.com>
2023-01-17 10:07:39 +0530

    Flutter

---
+++ Mobile Application Development using Flutter/Flutter Projects/app_project/lib/birthdaycard_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class BirthdatCard extends StatelessWidget {
+  const BirthdatCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text('Birthday Card')),
+      ),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset(
+            'assets/images/birthdaycardbackground.jpg',
+            fit: BoxFit.fill,
+          ),
+          Container(
+            margin: const EdgeInsets.only(bottom: 550, top: 50),
+            child: Image.asset(
+              'assets/images/game1.jpeg',
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(bottom: 200, left: 50, right: 50),
+            child: Image.asset(
+              'assets/images/download.png',
+            ),
+          ),
+          Image.asset(
+            'assets/images/transparentName.png',
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 500),
+            child: const Text(
+              '"Wishing you a day\n filled with happiness and\n a year filled with joy"',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontFamily: AutofillHints.birthday,
+                fontSize: 25,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- Mobile Application Development using Flutter/Flutter Projects/app_project/lib/home_page.dart
+++ Mobile Application Development using Flutter/Flutter Projects/app_project/lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:app_project/birthdaycard_page.dart';
 import 'package:app_project/dice_page.dart';
 import 'package:app_project/game_page.dart';
 import 'package:app_project/matrimony.dart';
@@ -122,6 +123,33 @@ class HomePage extends StatelessWidget {
                   },
                   child: const Text(
                     "Game",
+                    style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.blue),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Card(
+              elevation: 5,
+              child: Container(
+                padding: const EdgeInsets.all(20),
+                child: InkWell(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: ((context) {
+                          return const BirthdatCard();
+                        }),
+                      ),
+                    );
+                  },
+                  child: const Text(
+                    "Birthday Card",
                     style: TextStyle(
                         fontSize: 22,
                         fontWeight: FontWeight.w500,
